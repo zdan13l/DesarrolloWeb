5db5d607be83036c5956cb2411a254894098329a
Valentina Garcia <valentinagarciag089@gmail.com>
2026-09-06 22:51:50 -0500

    Creation and first dependencies

---
+++ sistema-gestion-api/src/main/java/com/sistema/gestion/sistema_gestion_api/SistemaGestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.gestion.sistema_gestion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionApiApplication.class, args);
+	}
+
+}
---
+++ sistema-gestion-api/src/test/java/com/sistema/gestion/sistema_gestion_api/SistemaGestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.gestion.sistema_gestion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
